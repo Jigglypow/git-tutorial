3620bee5d82c4dd385fd7df328de00726ff71930
Alex Hoffman <alxhoff@gmail.com>
2020-11-17 17:55:21 +0100

    number to words impl for code challenge 2

--- src/main.c
+++ src/main.c
@@ -2,11 +2,28 @@
  *  Copyright Jan 2020
  */
 #include <stdio.h>
-#include "espl_lib.h"    
+#include <stdbool.h>
+#include "espl_lib.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
-    printf("Hello ESPL");
-    printf("This is a new diff");
+    while (true){
+        printf("Pleaser enter a number: ");
+        int number;
+        scanf("%d", &number);
+        char *word = num_to_words(number);
+        printf("The number in words is %s.", word);
+
+        printf("\nDo you want to enter another number? [y, n] ");
+        char shouldContinue;
+        scanf("%s", &shouldContinue);
+        if(shouldContinue == 'y' || shouldContinue == 'Y'){
+            printf("\n");
+            continue;
+        }
+        break;        
+    }
+
     return 1;
 }
