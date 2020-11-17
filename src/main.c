00b6dddefc97bb1c2b17895a70756dff54041c8b
Alex Hoffman <alxhoff@gmail.com>
2020-11-17 12:58:49 +0100

    applied cmakelist patch for espl lib

--- src/main.c
+++ src/main.c
@@ -2,11 +2,11 @@
  *  Copyright Jan 2020
  */
 #include <stdio.h>
-
+#include "espl_lib.h"    
 
 int main(int argc, char *argv[]){
 
     printf("Hello ESPL");
-
+    printf("This is a new diff");
     return 1;
 }
